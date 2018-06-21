29c77ba7cf80714127e077e9083098430a7903a6
Brian Cilenti <briancilenti@gmail.com>
2018-06-21 14:02:02 -0700

    wtf happend

--- src/platform/routes/index.js
+++ src/platform/routes/index.js
@@ -1,5 +1,9 @@
 import React from "react";
-import { createBottomTabNavigator } from "react-navigation";
+import {
+  createBottomTabNavigator,
+  createStackNavigator,
+
+ } from "react-navigation";
 //import { Icon } from "react-native-elements";
 import Icon from 'react-native-vector-icons/Ionicons'
 //locals Drawer Navigation
@@ -63,3 +67,60 @@ export default createBottomTabNavigator({
     inactiveTintColor: 'black'
   }
 })
+
+
+//
+// export default createBottomTabNavigator({
+//   Home: {
+//     screen: Home,
+//     navigationOptions: {
+//       tabBarLabel: 'Dash',
+//       tabBarIcon: ({tintColor}) => (
+//         <Icon
+//           name="ios-home"
+//           color={tintColor}
+//           size={24}
+//         />
+//       )
+//     }
+//   },
+//   SignUp: {
+//     screen: SignUp,
+//     navigationOptions: {
+//       tabBarLabel: 'SignUp',
+//       tabBarIcon: ({tintColor}) => (
+//         <Icon
+//           name="ios-add"
+//           color={tintColor}
+//           size={24}
+//         />
+//       )
+//     }
+//   },
+//   Settings: {
+//     screen: Settings,
+//     navigationOptions: {
+//       tabBarLabel: 'Settings',
+//       tabBarIcon: ({tintColor}) => (
+//         <Icon
+//           name="ios-settings"
+//           color={tintColor}
+//           size={24}
+//         />
+//       )
+//     }
+//   }
+// },
+// {//router config
+//   initialRouteName: 'Home',
+//   order: ['SignUp','Settings', 'Home'],
+//   //nav options that apply to complete tab navigator
+//   navigationOptions: {
+//     tabBarVisible: true
+//   },
+//   tabBarOptions:{
+//     //activeTintColor supplies "tintColor" var above
+//     activeTintColor:'red',
+//     inactiveTintColor: 'black'
+//   }
+// })
