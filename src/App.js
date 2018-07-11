39f6ad73db2b8ee5f4764ed481c0dbbb5d82f431
Brian Cilenti <briancilenti@gmail.com>
2018-07-11 16:00:18 -0700

    updated endpoint

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ const authLink = setContext(async (_, { headers }) => {
 
 const client = new ApolloClient({
   link: authLink.concat(createUploadLink({
-    uri: 'https://itm-adv-server-fcldmxwfjj.now.sh' })
+    uri: 'https://itm-adv-server-qeijhnkumv.now.sh' })
   ),
   cache: new InMemoryCache(),
 });
